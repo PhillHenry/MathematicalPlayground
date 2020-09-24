c615406f324063f0935b414ca9a0acf7cce1588c
Phillip Henry <PhillHenry@gmail.com>
2020-09-24 17:43:58 +0100

    Nomenclature, test, documentation.

--- distributions/joint_gaussians.py
+++ distributions/joint_gaussians.py
@@ -3,12 +3,24 @@ import numpy as np
 import scipy.stats as stats
 
 
-def gaussian(xs, mu, s):
-    return stats.norm.pdf(xs, loc=mu, scale=s)
+def gaussian(xs, mu, sd):
+    return stats.norm.pdf(xs, loc=mu, scale=sd)
 
 
-def joint(mu1, mu2, o1, o2, xs, ys):
-    return gaussian(xs, mu1, o1) * gaussian(ys, mu2, o2)
+def joint(mu1, mu2, sd1, sd2, xs, ys):
+    r"""
+    (mu1 = 0; mu2 = 0; sd1 = 1; sd2 = 2) creates a peak curve at the origin that is symmetric under rotation.
+    Changing the mu values translate the peak
+    Changing the standard deviations changes the slope on the X- or Y- axis and eliminates rotational symmetry.
+    :param mu1: the mean of the first Gaussian
+    :param mu2: the mean of the second Gaussian
+    :param sd1: the standard deviation of the first Gaussian
+    :param sd2: the standard deviation of the second Gaussian
+    :param xs: array-like input to the first Gaussian
+    :param ys: array-like input to the second Gaussian
+    :return: a matrix like object that represents the values of the first Gaussian multiplied by the second
+    """
+    return gaussian(xs, mu1, sd1) * gaussian(ys, mu2, sd2)
 
 
 # graphics taken from
@@ -16,7 +28,7 @@ def joint(mu1, mu2, o1, o2, xs, ys):
 if __name__ == "__main__":
     support = np.arange(-3, 3, .2)
     xs, ys = np.meshgrid(support, support)
-    zs = joint(0, 0, 1, 1, xs, ys)
+    zs = joint(0, 0, 1, 1, xs, ys)  # note that changing the means moves the centre and the standard deviations the spread
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
--- tests/test_joint_gaussians.py
+++ tests/test_joint_gaussians.py
@@ -1,10 +1,16 @@
 from distributions import joint_gaussians
 from math import pi
+import numpy as np
 
 
 def test_gaussian():
     assert joint_gaussians.gaussian([0], 0, 1) == 1. / (2 * pi) ** 0.5
 
 
-
+def test_joint():
+    n = 10
+    support = np.arange(0, 10)
+    xs, ys = np.meshgrid(support, support)
+    assert np.shape(xs) == (n, n)
+    assert np.shape(ys) == (n, n)
 
