0c9c44212f1f34be7752c2393449c08e0f792fa1
Phillip Henry <PhillHenry@gmail.com>
2020-09-24 13:46:16 +0100

    3d plot

--- distributions/multvariate_normal.py
+++ distributions/multvariate_normal.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 
@@ -13,3 +14,17 @@ def bivariate_distn(mu1, mu2, s1, s2, range1, range2):
     p2 = gaussian(x2, mu2, s2)
     return p1 * p2
 
+
+if __name__ == "__main__":
+    step = .2
+    X = np.arange(-5., 5., step)
+    Y = np.arange(-5., 5., step)
+    X, Y = np.meshgrid(X, Y)
+    Z = np.sqrt(gaussian(X, 0, 1) + gaussian(Y, 0, 1))
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='hot', linewidth=0, antialiased=False)
+    ax.set_zlim(-1.01, 1.01)
+
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
