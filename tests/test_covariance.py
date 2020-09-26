a095dc0b3ea4698bed370f35442df0384b3ab6f2
Phillip Henry <PhillHenry@gmail.com>
2020-09-26 07:47:31 +0100

    Oops, correct dimensions.

--- tests/test_covariance.py
+++ tests/test_covariance.py
@@ -2,17 +2,26 @@ from math import pi
 import numpy as np
 import distributions.covariance as to_test
 
+xs = [1, 2, 3, 4]
+v = np.asmatrix(xs)
+m = np.stack((v + 1, v - 1))
+
 
 def test_mean_of_row_vector():
-    xs = np.asmatrix([1, 2, 3, 4])
-    assert(np.array_equal(to_test.mean_of(xs), xs))
+    assert(np.array_equal(to_test.mean_of(v), v))
+
+
+def test_mean_of_matrix():
+    assert(np.array_equal(to_test.mean_of(m), v))
 
 
 def test_covariance_of_row_vector():
-    xs = np.asmatrix([1, 2, 3, 4])
-    m = to_test.covariance_of(xs)
-    assert(np.shape(m) == (len(xs), len(xs)))
-    for i in range(len(xs)):
-        assert(m[i, i] == 1.)
+    c = to_test.covariance_of(v)
+    assert(np.shape(c) == (len(xs), len(xs)))
+    for i in range(len(v)):
+        assert(c[i, i] == 1.)
 
 
+def test_covariance_of_matrix():
+    c = to_test.covariance_of(m)
+    assert(np.shape(c) == (len(xs), len(xs)))
