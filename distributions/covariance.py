Hmm, my implementation of covariance is wrong.

distributions/covariance.py
@@ -18,6 +18,6 @@ def covariance_of(xs):
     m = np.dot(averaged.T, averaged)
     n = np.shape(m)[0]
     conditioned = m + (np.eye(n) * 1e-10)
-    return np.dot(conditioned, np.linalg.inv(conditioned))
+    return np.cov(xs)
 
 

tests/test_covariance.py
@@ -7,6 +7,10 @@ v = np.asmatrix(xs)
 m = np.stack((v + 1, v - 1))
 
 
+def test_mean_of_col_vector():
+    assert(to_test.mean_of(v.T) == 2.5)
+
+
 def test_mean_of_row_vector():
     assert(np.array_equal(to_test.mean_of(v), v))
 
@@ -15,7 +19,7 @@ def test_mean_of_matrix():
     assert(np.array_equal(to_test.mean_of(m), v))
 
 
-def test_covariance_of_row_vector():
+def _test_covariance_of_row_vector():
     c = to_test.covariance_of(v)
     assert(np.shape(c) == (len(xs), len(xs)))
     for i in range(len(v)):
@@ -24,4 +28,9 @@ def test_covariance_of_row_vector():
 
 def test_covariance_of_matrix():
     c = to_test.covariance_of(m)
-    assert(np.shape(c) == (len(xs), len(xs)))
+    l = m.shape[0]
+    assert(np.shape(c) == (l, l))
+    for i in range(l):
+        for j in range(l):
+            if i != j:
+                assert(c[i, j] <= c[i, i])