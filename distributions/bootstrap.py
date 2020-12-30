b734b036fe367631da454d028b1e491946000b9d
PhillHenry <phillhenry@gmail.com>
2020-12-30 06:45:12 +0000

    Plots.

--- distributions/bootstrap.py
+++ distributions/bootstrap.py
@@ -1,9 +1,10 @@
 import numpy as np
 from sklearn.utils import resample
 import matplotlib.pyplot as plt
+import math
 
 
-def bootstramp(data, n, n_samples):
+def bootstrap(data, n, n_samples):
     xs = []
     for _ in range(n):
         # see https://machinelearningmastery.com/a-gentle-introduction-to-the-bootstrap-method/
@@ -16,9 +17,9 @@ if __name__ == "__main__":
     mu = 0.
     sd = 1.
     n = 25
-    n_boots = 1000
+    n_boots = 10000
     data = np.random.normal(mu, sd, n)
-    boot = bootstramp(data, n_boots, n)
+    boot = bootstrap(data, n_boots, n)
 
     print("sample mean: %20f" % np.mean(data))
     print("sample sd:   %20f" % np.std(data))
@@ -28,10 +29,12 @@ if __name__ == "__main__":
     print("bootstrap mean mean: %12f" % np.mean(boot_means))
     print("bootstrap mean sd:   %12f" % np.mean(boot_sd))
 
-    plt.hist(boot_means, bins=int(n_boots/30))
-    # print("boot means: ", boot_means)
-    plt.show()
+    fig, (ax1, ax2) = plt.subplots(2)
+    ax1.hist(data, bins='auto')
+    ax1.set_title('Actual data for %d points' % n)
+    ax2.hist(boot_means, bins=int(math.log10(n_boots) * 10))
+    ax2.set_title('Bootstrapping with %d iterations' % n_boots)
 
-    #print('Bootstrap Sample: %s' % boot)
-    # out of bag observations
-    #oob = [x for x in data if x not in boot]
+    # print("boot means: ", boot_means)
+    plt.savefig("/tmp/boostrapping.png")
+    plt.show()
