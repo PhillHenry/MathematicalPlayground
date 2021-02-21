f6f8e610f84f06fc2e234455f06d7aba1c0910f3
PhillHenry <phillhenry@gmail.com>
2021-02-21 16:38:43 +0000

    Messing with t-tests.

--- distributions/spot_the_distribution.py
+++ distributions/spot_the_distribution.py
@@ -2,20 +2,30 @@ import numpy as np
 from scipy import stats
 
 
-def compare_tstats(a, b, N):
-    t2, p2 = stats.ttest_ind(a,b)
-    print("t = " + str(t2))
-    print("p = " + str(p2)) # ie, probability of rejecting the null hypothesis that the distributions are different
+def compare_tstats(a, b):
+    t2, p2 = stats.ttest_ind(a, b, equal_var=False)
+    # print("t = " + str(t2))
+    # print("p = " + str(p2))  # ie, probability of accepting the null hypothesis that the distributions are different
+    return t2, p2
+
+
+def compare_gaussian_to_exponentials():
+    n = 10
+    gaussians = np.random.normal(10, 1, n)
+    # print(gaussians)
+
+    exponentials = np.random.exponential(10, n)
+    # print(exponentials)
+
+    return compare_tstats(gaussians, exponentials)
 
 
 if __name__ == "__main__":
-    n = 10
-    gaussians = np.random.normal(10, 1, n)
-    print(gaussians)
-    binomials = np.random.binomial(10, 0.5, n)
-    print(binomials)
+    n_trials = 100
+    trials = map(lambda _: compare_gaussian_to_exponentials(), range(0, n_trials))
+    tps = np.array([*trials])
+    print(np.shape(tps))
+    # print(tps)
+    mean_p = np.mean(tps[:, 1])
+    print("mean p = {}".format(mean_p))
 
-    exponentials = np.random.exponential(10, n)
-    print(exponentials)
-
-    compare_tstats(gaussians, exponentials, n)
