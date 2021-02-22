189987ae17415478168f46891c04311c6d0de720
Phillip Henry <PhillHenry@gmail.com>
2021-02-22 13:47:52 +0000

    Kolomgorov Smirnov.

--- distributions/spot_the_distribution.py
+++ distributions/spot_the_distribution.py
@@ -1,11 +1,16 @@
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
+from scipy.stats import ks_2samp
 
 
 def compare_tstats(a, b):
     t2, p2 = stats.ttest_ind(a, b, equal_var=False)
     return t2, p2
+
+
+def compare_kolmogorov_smirnov(a, b):
+    return ks_2samp(a, b).pvalue
 
 
 def compare_gaussian_to_exponentials(n=1000):
@@ -29,10 +34,20 @@ def plot(ps, xs, ys):
     plt.show()
 
 
-if __name__ == "__main__":
-    n_trials = 100
-    trials = list(map(lambda _: compare_gaussian_to_exponentials(), range(0, n_trials)))
+def make_comparison(n_trials, fn):
+    trials = list(map(lambda _: fn(), range(0, n_trials)))
     m = np.asarray([*trials], object)
     metrics, xs, ys = np.transpose(m)
+    return metrics, xs, ys
+
+
+def t_test(n_trials=100):
+    metrics, xs, ys = make_comparison(n_trials, compare_gaussian_to_exponentials)
     tps = np.array([*metrics])
-    plot(tps[:, 1], xs, ys)
+    return tps[:, 1], xs, ys
+
+
+if __name__ == "__main__":
+    n_trials = 100
+    ps, xs, ys = t_test(n_trials)
+    plot(ps, xs, ys)
--- tests/test_spot_the_distribution.py
+++ tests/test_spot_the_distribution.py
@@ -1,4 +1,5 @@
 import distributions.spot_the_distribution as to_test
+import numpy as np
 
 
 def test_compare_gaussian_to_exponentials():
@@ -6,3 +7,14 @@ def test_compare_gaussian_to_exponentials():
     comparison, xs, ys = to_test.compare_gaussian_to_exponentials(n)
     assert(len(xs) == n)
     assert(len(ys) == n)
+
+
+def test_compare_kolmogorov_smirnov():
+    np.random.seed(12345678)
+    n = 10000
+    x = np.random.normal(0, 1, n)
+    y = np.random.normal(0, 1, n)
+    z = np.random.normal(1.1, 0.9, n)
+    assert(to_test.compare_kolmogorov_smirnov(x, y) > 0.05)
+
+
