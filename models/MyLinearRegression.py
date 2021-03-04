ca4cebcd96725ac040ed6ea0fc865274294b02d8
Phillip Henry <PhillHenry@gmail.com>
2021-03-04 11:25:40 +0000

    Manually reads SVM. I like doing things the hard way...

---
+++ models/MyLinearRegression.py
@@ -0,0 +1,40 @@
+import sys
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+def parse_line(xs):
+    elements = []
+    for x in xs:
+        pos, val = x.split(":")
+        elements.append(val)
+    return elements
+
+
+def load_svm_file(file):
+    print("Loading {}".format(file))
+    actual = []
+    obs = []
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            elements = line.strip().split(" ")
+            actual.append(elements[0])
+            obs.append(parse_line(elements[1:]))
+        f.close()
+    return obs, actual
+
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    xs, ys = load_svm_file(file)
+    assert(len(xs) == len(ys))
+    print("Loaded {} lines with actual values that look like {} and observations like:\n{}"
+          .format(len(xs), ys[0], xs[0]))
+
+
+
+
+
+
+
