0d1aa675abb91d99097d6796d2563290f2b28ed9
Phillip Henry <PhillHenry@gmail.com>
2021-03-04 14:29:54 +0000

    Two params.

--- models/hyperparameter_tuning.py
+++ models/hyperparameter_tuning.py
@@ -12,15 +12,14 @@ class LGTuner(object):
         self.ys = ys
 
     def objective(self, args):
-        alpha = args
-        print("alpha = {}".format(alpha))
-        reg = ElasticNet(alpha=alpha).fit(xs, ys)
+        l1_ratio, alpha = args
+        reg = ElasticNet(alpha=alpha, l1_ratio=l1_ratio).fit(xs, ys)
         return reg.score(xs, ys)
 
     def tune(self):
         space = hp.choice('a',
                           [
-                              hp.uniform('alpha', 0, 1.0)
+                              (hp.uniform('l1_ratio', 0, 1.0), hp.lognormal('alpha', 0.01, 1.0))
                           ])
         best = fmin(self.objective, space, algo=tpe.suggest, max_evals=100)
         return best
