c99f1af75c80152525d0835601f6828a84d849ad
Phillip Henry <PhillHenry@gmail.com>
2021-03-04 13:56:01 +0000

    Refactor.

--- models/MyLinearRegression.py
+++ models/MyLinearRegression.py
@@ -1,28 +1,6 @@
 import sys
 from sklearn.linear_model import LinearRegression
-
-
-def parse_line(xs):
-    elements = []
-    for x in xs:
-        pos, val = x.split(":")
-        elements.append(float(val))
-    return elements
-
-
-def load_svm_file(file):
-    print("Loading {}".format(file))
-    actual = []
-    obs = []
-    with open(file, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            elements = line.strip().split(" ")
-            actual.append(float(elements[0]))
-            obs.append(parse_line(elements[1:]))
-        f.close()
-    return obs, actual
-
+from data_loader import load_svm_file
 
 if __name__ == "__main__":
     file = sys.argv[1]
---
+++ models/data_loader.py
@@ -0,0 +1,21 @@
+
+def parse_line(xs):
+    elements = []
+    for x in xs:
+        pos, val = x.split(":")
+        elements.append(float(val))
+    return elements
+
+
+def load_svm_file(file):
+    print("Loading {}".format(file))
+    actual = []
+    obs = []
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            elements = line.strip().split(" ")
+            actual.append(float(elements[0]))
+            obs.append(parse_line(elements[1:]))
+        f.close()
+    return obs, actual
