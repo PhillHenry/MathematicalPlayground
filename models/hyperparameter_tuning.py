354f8ab8ebd756c3a38b6ba10d1919464138827e
Phillip Henry <PhillHenry@gmail.com>
2021-03-04 14:18:49 +0000

    First (failing) attempt at Hyperopt.

---
+++ models/hyperparameter_tuning.py
@@ -0,0 +1,32 @@
+from hyperopt import hp
+from data_loader import load_svm_file
+import sys
+from hyperopt import fmin, tpe, space_eval
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet
+
+
+class LGTuner(object):
+    def __init__(self, xs, ys):
+        self.xs = xs
+        self.ys = ys
+
+    def objective(self, args):
+        alpha = args
+        reg = alpha().fit(xs, ys)
+        return reg.score(xs, ys)
+
+    def tune(self):
+        space = hp.choice('a',
+                          [
+                              ('case 2', hp.uniform('c2', 0, 1.0))
+                          ])
+        best = fmin(self.objective, space, algo=tpe.suggest, max_evals=100)
+        return best
+
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    xs, ys = load_svm_file(file)
+    tuner = LGTuner(xs, ys)
+    print(tuner.tune())
