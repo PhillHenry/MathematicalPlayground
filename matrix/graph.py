3f4612ac78dcb72ef7928ef7cc1392a5bd25a745
Phillip Henry <PhillHenry@gmail.com>
2021-04-23 12:04:47 +0100

    More realistic values. Added a row for the healthy population.

--- matrix/graph.py
+++ matrix/graph.py
@@ -5,37 +5,41 @@ np.set_printoptions(precision=3)
 # np.set_printoptions(suppress=True)
 
 
-def transitions(n=5):
-    a = np.random.rand(n, n)
-    max_wait = 3
+def transitions(n=6):
+    a = np.random.rand(n, n) #.astype("float64")
+    max_wait = min(n // 2, n - 3)
+    # waiting weeks can't go backwards
     for i in range(max_wait):
         for j in range(max_wait):
             if j <= i:
                 a[i][j] = 0.
+    # other states cannot go into waiting weeks
+    for i in range(0, max_wait):
+        for j in range(n - max_wait, n):
+            a[j][i] = 0.
+    # ... except the majority of the population
+    p_diagnosis = 1e-13
+    a[n-1, :] = 0.
+    a[:, n-1] = 0.
+    a[n-1, 0] = p_diagnosis
+    a[n-1, n-1] = 1 - p_diagnosis
     row_sums = a.sum(axis=1)
     return a / row_sums[:, np.newaxis]
+    # return a / a.sum(axis=1).sum(axis=2)
 
 
 if __name__ == "__main__":
     x = transitions()
 
-    print(x)
+    print(f"initial matrix:\n{x}")
 
     eigen_vals, eigen_vecs = np.linalg.eig(x)
-    print(eigen_vecs)
-    print(eigen_vals)
+    print(f"Eigenvectors:\n{eigen_vecs}")
+    print(f"Eigenvalues:\n{eigen_vals}")
 
-    for _ in range(30):
+    iterations = 30
+    for _ in range(iterations):
         x = np.dot(x, x)
 
-    print(x)
+    print(f"After {iterations} the matrix looks like:\n{x}")
 
-    n = len(eigen_vecs)
-    Q = eigen_vecs
-    lambda_inf = np.zeros([n, n])
-    lambda_inf[0][0] = 1.
-    via_eig = np.dot(Q, np.dot(lambda_inf, np.linalg.inv(Q)))
-    tol = 1e-16
-    via_eig.real[abs(via_eig.real) < tol] = 0.0
-    via_eig.imag[abs(via_eig.imag) < tol] = 0.0
-    print(via_eig.real)
