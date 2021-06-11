b4b215190518c79051eddc7855e12b21ea037f87
PhillHenry <phillhenry@gmail.com>
2021-06-11 13:50:29 +0100

    One hot encoding and singularity

---
+++ matrix/standardisation_vs_singular.py
@@ -0,0 +1,33 @@
+import numpy as np
+import random
+
+
+def make_fake_1hot_encodings(n_rows=1000, n_categories=4, n_cardinality=5) -> np.ndarray:
+    n_cols = n_categories * n_cardinality
+    m = np.zeros([n_rows, n_cols])
+    for i in range(n_rows):
+        for j in range(n_categories):
+            index = int(random.random() * (n_cardinality + 1))
+            if index < n_cardinality:
+                m[i][(j * n_cardinality) + index] = 1.
+    return m
+
+
+def square(m):
+    return np.dot(m, m.transpose())
+
+
+def standardize(m: np.ndarray) -> np.ndarray:
+    return (m - m.mean())/(m.std())
+
+
+if __name__ == "__main__":
+    m = make_fake_1hot_encodings()
+    m_squared = square(m)
+
+    print(f"det(m_squared) = {np.linalg.det(m_squared)}")
+
+    m_standardized = standardize(m)
+    m_standardized_squared = square(m_standardized)
+
+    print(f"det(m_squared) = {np.linalg.det(m_standardized_squared)}")
