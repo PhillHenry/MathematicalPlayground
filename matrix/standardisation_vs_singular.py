6aa74b3f437752d9fb1c93fc5ec836801af1336c
PhillHenry <phillhenry@gmail.com>
2021-06-12 11:48:25 +0100

    Refactor.

--- matrix/standardisation_vs_singular.py
+++ matrix/standardisation_vs_singular.py
@@ -7,17 +7,16 @@ def make_fake_1hot_encodings(n_rows=1000,
                              n_cardinality=5,
                              drop_last=True) -> np.ndarray:
     if drop_last:
-        drop_last = 1
-        n_cardinality = n_cardinality - 1
+        correction = 1
     else:
-        drop_last = 0
-    n_cols = n_categories * n_cardinality
+        correction = 0
+    n_cols = n_categories * (n_cardinality - correction)
     m = np.zeros([n_rows, n_cols])
     for i in range(n_rows):
         for j in range(n_categories):
-            index = int(random.random() * (n_cardinality + drop_last))
-            if index < n_cardinality:
-                m[i][(j * n_cardinality) + index] = 1.
+            index = int(random.random() * n_cardinality)
+            if index < (n_cardinality - correction):
+                m[i][(j * (n_cardinality - correction)) + index] = 1.
     return m
 
 
@@ -41,7 +40,7 @@ def invert_and_standardize(m):
 
 
 if __name__ == "__main__":
-    m = make_fake_1hot_encodings()
+    m = make_fake_1hot_encodings(drop_last=True)
     invert_and_standardize(m)
     m = make_fake_1hot_encodings(drop_last=False)
     invert_and_standardize(m)
--- tests/test_fake_1hot_encodings.py
+++ tests/test_fake_1hot_encodings.py
@@ -1,10 +1,18 @@
 from matrix.standardisation_vs_singular import make_fake_1hot_encodings
+import numpy as np
+
+
+def check_columns_contain_ones(m):
+    n_cols = np.shape(m)[1]
+    for i in range(n_cols):
+        assert sum(m[:, i]) != 0
 
 
 def test_every_row_has_a_1_for_each_category_when_no_drop_last():
     n_categories = 4
     n_cardinality = 5
     m = make_fake_1hot_encodings(n_categories=n_categories, n_cardinality=n_cardinality, drop_last=False)
+    check_columns_contain_ones(m)
     for row in m:
         assert sum(row) == n_categories
         assert len(row) == n_categories * n_cardinality
@@ -14,6 +22,7 @@ def test_fewer_rows_when_drop_last():
     n_categories = 4
     n_cardinality = 5
     m = make_fake_1hot_encodings(n_categories=n_categories, n_cardinality=n_cardinality, drop_last=True)
+    check_columns_contain_ones(m)
     for row in m:
         assert len(row) == n_categories * (n_cardinality - 1)
         assert sum(row) <= n_categories
