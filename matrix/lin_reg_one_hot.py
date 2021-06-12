bfe2299da983e0a79e6dda11353e5436531ca772
PhillHenry <phillhenry@gmail.com>
2021-06-12 12:18:11 +0100

    Cleaned.

--- matrix/lin_reg_one_hot.py
+++ matrix/lin_reg_one_hot.py
@@ -1,8 +1,8 @@
-import numpy as np
 from data.one_hot_encodings import make_fake_1hot_encodings
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn import linear_model
+from sklearn import linear_model
 
+from data.one_hot_encodings import make_fake_1hot_encodings
 
 if __name__ == "__main__":
     x = make_fake_1hot_encodings()
@@ -12,11 +12,9 @@ if __name__ == "__main__":
         y = [a * (b + 1) for a, b in zip(row, range(len(row)))]
         ys.append(sum(y))
 
-    print("y shape ", np.shape(x))
-    print("y shape ", np.shape(y))
-
     regr = linear_model.LinearRegression()
-    regr.fit(x, ys)
+    est = regr.fit(x, ys)
 
     # The coefficients
     print('Coefficients: \n', regr.coef_)
+    print(est.summary())
