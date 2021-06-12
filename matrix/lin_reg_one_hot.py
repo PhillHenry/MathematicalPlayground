c853f655c9bc5a05f2905833f308e6b1fb17f458
PhillHenry <phillhenry@gmail.com>
2021-06-12 12:43:44 +0100

    Different results when not dropping last.

--- matrix/lin_reg_one_hot.py
+++ matrix/lin_reg_one_hot.py
@@ -1,20 +1,28 @@
-from data.one_hot_encodings import make_fake_1hot_encodings
+import statsmodels.api as sm
 from sklearn import linear_model
-from sklearn import linear_model
 
 from data.one_hot_encodings import make_fake_1hot_encodings
 
-if __name__ == "__main__":
-    x = make_fake_1hot_encodings()
 
+def make_y(x):
     ys = []
     for row in x:
         y = [a * (b + 1) for a, b in zip(row, range(len(row)))]
         ys.append(sum(y))
+    return ys
 
+
+def no_drop_last():
+    x = make_fake_1hot_encodings(drop_last=True)
+    ys = make_y(x)
     regr = linear_model.LinearRegression()
     est = regr.fit(x, ys)
+    print(f'{len(regr.coef_)} Coefficients: \n', regr.coef_)
 
-    # The coefficients
-    print('Coefficients: \n', regr.coef_)
-    print(est.summary())
+
+if __name__ == "__main__":
+    no_drop_last()
+    # X2 = sm.add_constant(x)
+    # est = sm.OLS(y, X2)
+    # est2 = est.fit()
+    # print(est2.summary())
