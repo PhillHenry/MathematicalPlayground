e07022b56c8f84a426d8dc98171910b96fedf933
PhillHenry <phillhenry@gmail.com>
2021-06-12 13:39:02 +0100

    Test

--- matrix/lin_reg_one_hot.py
+++ matrix/lin_reg_one_hot.py
@@ -1,7 +1,7 @@
 import statsmodels.api as sm
 from sklearn import linear_model
 import numpy as np
-
+from sklearn.metrics import mean_squared_error, r2_score
 from data.one_hot_encodings import make_fake_1hot_encodings
 
 
@@ -14,11 +14,22 @@ def make_y(x):
 
 
 def no_drop_last(regr, drop_last=True):
-    x = make_fake_1hot_encodings(drop_last=drop_last)
+    n_rows = 1000
+    x = make_fake_1hot_encodings(drop_last=drop_last, n_rows=n_rows)
     ys = make_y(x)
-    est = regr.fit(x, ys)
+    n_train = int(n_rows * 0.8)
+    train_x = x[:n_train, :]
+    train_y = ys[:n_train]
+    est = regr.fit(train_x, train_y)
+    test_x = x[n_train:, :]
+    test_y = ys[n_train:]
+    y_pred = regr.predict(test_x)
     print(f'{len(regr.coef_)} Coefficients: \n', ['%.3f' % x for x in regr.coef_])
     print("intercept", regr.intercept_)
+    print('Mean squared error: %.2f'
+          % mean_squared_error(test_y, y_pred))
+    print('Coefficient of determination: %.2f'
+          % r2_score(test_y, y_pred))
 
 
 def alpha_intercepts(m):
