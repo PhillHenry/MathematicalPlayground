aaf6677cb137bb1a444ed6c492d86b9b523d2964
PhillHenry <phillhenry@gmail.com>
2021-06-12 13:26:43 +0100

    Different models.

--- matrix/lin_reg_one_hot.py
+++ matrix/lin_reg_one_hot.py
@@ -1,5 +1,6 @@
 import statsmodels.api as sm
 from sklearn import linear_model
+import numpy as np
 
 from data.one_hot_encodings import make_fake_1hot_encodings
 
@@ -12,17 +13,24 @@ def make_y(x):
     return ys
 
 
-def no_drop_last():
-    x = make_fake_1hot_encodings(drop_last=True)
+def no_drop_last(regr, drop_last=True):
+    x = make_fake_1hot_encodings(drop_last=drop_last)
     ys = make_y(x)
-    regr = linear_model.LinearRegression()
     est = regr.fit(x, ys)
     print(f'{len(regr.coef_)} Coefficients: \n', regr.coef_)
+    print("intercept", regr.intercept_)
+
+
+def alpha_intercepts(m):
+    for intercept in [True, False]:
+        for drop in [True, False]:
+            print(f"\n{m}, fit_interecpt = {intercept}, drop_last = {drop}")
+            no_drop_last(m(alpha=.01, fit_intercept=intercept), drop_last=drop)
 
 
 if __name__ == "__main__":
-    no_drop_last()
-    # X2 = sm.add_constant(x)
-    # est = sm.OLS(y, X2)
-    # est2 = est.fit()
-    # print(est2.summary())
+    np.set_printoptions(precision=3)
+    no_drop_last(linear_model.LinearRegression(fit_intercept=True))
+    no_drop_last(linear_model.LinearRegression(fit_intercept=False))
+    alpha_intercepts(linear_model.Ridge)
+    alpha_intercepts(linear_model.Lasso)
