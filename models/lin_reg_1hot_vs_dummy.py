f0c59d6e85da165b9b99923738642fa713b524ec
PhillHenry <phillhenry@gmail.com>
2021-06-14 06:17:18 +0100

    refactor.

--- models/lin_reg_1hot_vs_dummy.py
+++ models/lin_reg_1hot_vs_dummy.py
@@ -24,21 +24,25 @@ def skip_every(n, xs):
     return [x for i, x in enumerate(xs) if (i + 1) % n != 0]
 
 
+def num_non_increasing(xs, skip):
+    return len([x for x in deltas(skip_every(skip, xs)) if x < 0])
+
+
 def compare_1hot_vs_dummy():
     n_rows = 1000
     n_categories = 4
     n_cardinality = 5
     m = make_fake_1hot_encodings(drop_last=False, n_rows=n_rows, n_categories=n_categories, n_cardinality=n_cardinality)
     m_dropped = drop_last(m, n_categories, n_cardinality)
-    for error in [10, 100, 1000]:
+    for error in [0, 10, 100, 1000]:
         ys = make_y(m, error=error)
         for intercept in [True, False]:
             m_error, m_coeffs = train_and_check(m, ys, n_rows, intercept)
             m_dropped_error, m_dropped_coeffs = train_and_check(m_dropped, ys, n_rows, intercept)
             delta_error = m_error - m_dropped_error
             print(("=== difference in error %.4f (%.4f)" % (delta_error, delta_error * 100 / m_error)))
-            print(f"non increasing coefficients: {len([x for x in deltas(skip_every(n_cardinality, m_coeffs)) if x <= 0])}")
-            print(f"non increasing dropped coefficients: {len([x for x in deltas(skip_every(n_cardinality, m_dropped_coeffs)) if x <= 0])}")
+            print(f"non increasing coefficients: {num_non_increasing(m_coeffs, n_cardinality)}")
+            print(f"non increasing dropped coefficients: {num_non_increasing(m_dropped_coeffs, n_cardinality)}")
 
 
 if __name__ == "__main__":
