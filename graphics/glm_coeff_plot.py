3fe38167f2ca3e7e0493ee4a44c4275eb302c8c0
Phillip Henry <PhillHenry@gmail.com>
2021-12-10 13:38:46 +0000

    Correct IMD Log coefficients.

--- graphics/glm_coeff_plot.py
+++ graphics/glm_coeff_plot.py
@@ -8,15 +8,12 @@ import re as re
 
 
 def interesting(fun):
-    return fun[~fun["feature"].str.contains("intercept") &
-        ~fun["feature"].str.contains("arrival") &
-        ~fun["feature"].str.contains("acuity") &
-        ~fun["feature"].str.contains("priority") &
-        ~fun["feature"].str.contains("RTT")]
+    return fun[fun["feature"].str.contains("ethnic") |
+        ~fun["feature"].str.contains("deprivation")]
 
 
 def plot(df, c, label):
-    plt.scatter(df.coefficients, np.log(df.standard_error), c=c, label=label)
+    plt.scatter(np.log(df.coefficients), np.log(df.standard_error), c=c, label=label)
 
 
 if __name__ == '__main__':
@@ -26,11 +23,14 @@ if __name__ == '__main__':
         label = re.sub("\..*", "", label)
         print(f"Reading {file} and giving it label {label}")
         df = pd.read_csv(file, sep="\t")
+        # if "1hot" not in label:
+        # 'correct' IMDs if they're deciles
+        df["coefficients"] = np.where(df["feature"].str.contains("imd19_decile"), df["coefficients"] * 10, df["coefficients"])
         cleaned = df[(df["p_values"] < 0.05)
                      & ((df["coefficients"] > 0.1) | (df["coefficients"] < -0.1))]
         significant = interesting(cleaned)
         plot(significant, colours[i], label)
-    plt.xlabel("Coefficients")
+    plt.xlabel("Log coefficients")
     plt.ylabel("Log standard error")
     plt.legend()
     plt.show()
