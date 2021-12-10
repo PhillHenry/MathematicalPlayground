da636ba30a483a0d50d265d8a121aaaaf9fd5e16
Phillip Henry <PhillHenry@gmail.com>
2021-12-10 13:49:48 +0000

    Correct log using absolute values.

--- graphics/glm_coeff_plot.py
+++ graphics/glm_coeff_plot.py
@@ -7,13 +7,17 @@ import sys
 import re as re
 
 
+COEFFICIENTS = "coefficients"
+
+
 def interesting(fun):
     return fun[fun["feature"].str.contains("ethnic") |
-        ~fun["feature"].str.contains("deprivation")]
+               ~fun["feature"].str.contains("deprivation") |
+               ~fun["feature"].str.contains("decile")]
 
 
 def plot(df, c, label):
-    plt.scatter(np.log(df.coefficients), np.log(df.standard_error), c=c, label=label)
+    plt.scatter(np.log(df[COEFFICIENTS].abs()), np.log(df.standard_error), c=c, label=label)
 
 
 if __name__ == '__main__':
@@ -25,12 +29,13 @@ if __name__ == '__main__':
         df = pd.read_csv(file, sep="\t")
         # if "1hot" not in label:
         # 'correct' IMDs if they're deciles
-        df["coefficients"] = np.where(df["feature"].str.contains("imd19_decile"), df["coefficients"] * 10, df["coefficients"])
+        df[COEFFICIENTS] = np.where(df["feature"].str.contains("imd19_decile"), df[
+            COEFFICIENTS] * 10, df[COEFFICIENTS])
         cleaned = df[(df["p_values"] < 0.05)
-                     & ((df["coefficients"] > 0.1) | (df["coefficients"] < -0.1))]
+                     & ((df[COEFFICIENTS] > 0.1) | (df[COEFFICIENTS] < -0.1))]
         significant = interesting(cleaned)
         plot(significant, colours[i], label)
-    plt.xlabel("Log coefficients")
+    plt.xlabel("Log |coefficients|")
     plt.ylabel("Log standard error")
     plt.legend()
     plt.show()
