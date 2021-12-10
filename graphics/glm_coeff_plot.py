27a1b6e9b6e763f71a6ba767d9543236ee5bd16c
Phillip Henry <PhillHenry@gmail.com>
2021-12-10 12:45:52 +0000

    Scatter plot of Pandas data.

--- graphics/glm_coeff_plot.py
+++ graphics/glm_coeff_plot.py
@@ -13,17 +13,18 @@ def interesting(fun):
         ~fun["feature"].str.contains("RTT")]
 
 
-def plot(df):
-    # fig, ax = plt.subplots(figsize=(12,8))
-    # df.plot(ax=ax,legend=True)
-    df.plot(kind='scatter', x='coefficients', y=['standard_error']).plot(figsize=(12,8))
-    plt.show()
+def plot(df, c):
+    plt.scatter(df.coefficients, df.standard_error, c=c)
 
 
 if __name__ == '__main__':
-    for file in sys.argv[1:]:
+    colours = ['r', 'b']
+    for i, file in enumerate(sys.argv[1:]):
         print(f"Reading {file}")
         df = pd.read_csv(file, sep="\t")
         cleaned = df[(df["p_values"] < 0.05) & ((df["coefficients"] > 0.1) | (df["coefficients"] < -0.1))]
         significant = interesting(cleaned)
-        plot(significant)
+        plot(significant, colours[i])
+    plt.xlabel("Coefficients")
+    plt.ylabel("Standard error")
+    plt.show()
