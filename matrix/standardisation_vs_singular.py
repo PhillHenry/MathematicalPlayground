9dd896a2ca752e1e8b311b425a4289b830b0ae91
Phillip Henry <PhillHenry@gmail.com>
2022-01-23 09:15:45 +0000

    Refactoring.

--- matrix/standardisation_vs_singular.py
+++ matrix/standardisation_vs_singular.py
@@ -3,7 +3,7 @@ import numpy as np
 from data.one_hot_encodings import make_fake_1hot_encodings, make_y
 
 
-def square(m):
+def mmT(m):
     return np.dot(m, m.transpose())
 
 
@@ -16,16 +16,21 @@ def invert_and_standardize(m):
     Formula for calculating the co-efficients in a linear regression is:
     β=(X′X)−1X′Y
     '''
-    print(f"det((m'm)^-1)                            = {np.linalg.det(np.linalg.inv(np.dot(m.transpose(), m)))}")
-    print(f"det(m_squared)                           = {np.linalg.det(square(m))}")
+    print(f"det((m'm))                               = {np.linalg.det(mTm(m))}")
+    print(f"det((m'm)^-1)                            = {np.linalg.det(np.linalg.inv(mTm(m)))}")
+    print(f"det(mm')                                 = {np.linalg.det(mmT(m))}")
     msg = "det((mm')^-1)                            = "
     try:
-        det_invert = np.linalg.det(np.linalg.inv(square(m)))
+        det_invert = np.linalg.det(np.linalg.inv(mmT(m)))
         print(msg, det_invert)  # blows up because mm' is singular
     except Exception as e:
         print(f"{msg}Could not find det((mm')^-1. Error = {e}")
-    print(f"det(standardized(m) * standardized(m).T) = {np.linalg.det(square(standardize(m)))}")
-    print(f"det(standardized(m m.T)                  = {np.linalg.det(standardize(square(m)))}")
+    print(f"det(standardized(m) * standardized(m).T) = {np.linalg.det(mmT(standardize(m)))}")
+    print(f"det(standardized(m m.T)                  = {np.linalg.det(standardize(mmT(m)))}")
+
+
+def mTm(m):
+    return np.dot(m.transpose(), m)
 
 
 def add_value_col(m):
@@ -44,7 +49,8 @@ def det_of_square_matrix(drop_last: bool):
                                              n_rows=n_rows,
                                              n_categories=n_categories,
                                              n_cardinality=5)
-    print(f"{np.linalg.det(square_matrix)} for matrix of shape {np.shape(square_matrix)}")
+    print(f"determinant(m)   = {np.linalg.det(square_matrix)} for matrix of shape {np.shape(square_matrix)}")
+    print(f"determinant(mTm) = {np.linalg.det(mTm(square_matrix))} for matrix of shape {np.shape(square_matrix)}")
 
 
 if __name__ == "__main__":
@@ -66,14 +72,14 @@ if __name__ == "__main__":
                                                n_categories=n_categories,
                                                n_cardinality=n_cardinality,
                                                n_rows=((n_categories * n_cardinality) + 1)))
-    print(f"\ncoincidentally square matrix {np.shape(m)} det = {np.linalg.det(square(m))}")
+    print(f"\ncoincidentally square matrix {np.shape(m)} det = {np.linalg.det(mmT(m))}")
     m = add_value_col(make_fake_1hot_encodings(drop_last=True,
                                                n_categories=n_categories,
                                                n_cardinality=n_cardinality,
                                                n_rows=((n_categories * (n_cardinality - 1)) + 1)))
-    print(f"\ncoincidentally square matrix {np.shape(m)} det = {np.linalg.det(square(m))}")
+    print(f"\ncoincidentally square matrix {np.shape(m)} det = {np.linalg.det(mmT(m))}")
 
     det_of_square_matrix(False)
     det_of_square_matrix(True)
     print("\nrandom matrix")
-    print(np.linalg.det(np.random.rand(100, 100)))
+    print(f"det(m) = {np.linalg.det(np.random.rand(100, 100))}")
