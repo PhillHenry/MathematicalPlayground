da3999d603b26e2abd70e89235ffb19643ea9520
PhillHenry@gmail.com <PhillHenry@gmail.com>
2022-10-24 09:33:15 +0100

    From https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.correlation_lags.html

---
+++ series/cross_series_correlation.py
@@ -0,0 +1,11 @@
+from scipy import signal
+import numpy as np
+from numpy.random import default_rng
+
+rng = default_rng()
+x = rng.standard_normal(1000)
+y = np.concatenate([rng.standard_normal(100), x])
+correlation = signal.correlate(x, y, mode="full")
+lags = signal.correlation_lags(x.size, y.size, mode="full")
+lag = lags[np.argmax(correlation)]
+print(lag)
