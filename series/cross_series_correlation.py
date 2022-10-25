b73da21887340b4ad464fca4c7b94f51f67207e7
PhillHenry@gmail.com <PhillHenry@gmail.com>
2022-10-25 08:53:34 +0100

    Added noise

--- series/cross_series_correlation.py
+++ series/cross_series_correlation.py
@@ -2,10 +2,35 @@ from scipy import signal
 import numpy as np
 from numpy.random import default_rng
 
+
+real_lag = 100
+
+
+def print_sample(xs: list):
+    first = xs[:10]
+    last = xs[-10:]
+    mid_start = real_lag - 5
+    mid_end = real_lag + 5
+    mid = xs[mid_start:mid_end]
+    print()
+    print(first)
+    print(mid)
+    print(last)
+
+
 rng = default_rng()
 x = rng.standard_normal(1000)
-y = np.concatenate([rng.standard_normal(100), x])
+noise = rng.standard_normal(1000)
+y = np.concatenate([rng.standard_normal(real_lag), x + noise])
+
 correlation = signal.correlate(x, y, mode="full")
-lags = signal.correlation_lags(x.size, y.size, mode="full")
-lag = lags[np.argmax(correlation)]
-print(lag)
+print_sample(correlation)
+max_correlation = np.argmax(correlation)
+print(f"max_correlation = {max_correlation}")
+
+lag_indices = signal.correlation_lags(x.size, y.size, mode="full")
+print_sample(lag_indices)
+
+lag = lag_indices[max_correlation]
+print(f"lag = {lag}")
+assert lag == -real_lag
