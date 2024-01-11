69bf63407f80e2c1bd09833095ffbbb4a2d75088
PhillipHenry <PhillHenry@gmail.com>
2024-01-11 10:14:36 +0000

    Sparse

--- graphics/zorder_2d.py
+++ graphics/zorder_2d.py
@@ -1,10 +1,6 @@
-import matplotlib.pyplot as plt
-import matplotlib
-
-from sorting.zorder import interleave
 import matplotlib
 import matplotlib.pyplot as plt
-
+import numpy as np
 from sorting.zorder import interleave
 
 
@@ -15,7 +11,8 @@ def draw_curve(ax, num_bits):
       z = interleave(i, j)
       z_to_loc[z] = [i, j]
 
-  locs = [z_to_loc[z] for z in sorted(z_to_loc)]
+  indices = sorted(np.random.choice(list(z_to_loc.keys()), len(z_to_loc)//2, replace=False))
+  locs = [z_to_loc[z] for z in sorted(indices)]
 
   # Choose pretty colors.
   cmap = matplotlib.cm.get_cmap('rainbow')
@@ -36,11 +33,11 @@ def draw_curve(ax, num_bits):
     for j in range(int(2 ** num_bits)):
       plt.plot(i, j, "y+")
   ax.set_aspect('equal')
-  ax.set_title("Sparse ")
+  ax.set_title("Sparse")
   ax.set_xlabel('dimension 1')
   ax.set_ylabel('dimension 2')
 
 # see https://github.com/PrincetonLIPS/numpy-hilbert-curve/blob/main/README.md
 draw_curve(plt.axes(), 5)
-plt.savefig('/home/henryp/Pictures/zorder_2d.png', bbox_inches='tight')
+plt.savefig('/home/henryp/Pictures/zorder_sparse_2d.png', bbox_inches='tight')
 plt.show()
